7da85e1ecf4645bfc98d19514186d48a858c6e4a
Diego Restrepo <restrepomesadiego@gmail.com>
2021-12-11 14:02:45 -0500

    test(back): add test for polf1
    - test comparisonPolynomialF1WithPolynomialF2

--- src/test/java/com/polynomialprogram/app/PolynomialF1Test.java
+++ src/test/java/com/polynomialprogram/app/PolynomialF1Test.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
-import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -27,6 +26,28 @@ public class PolynomialF1Test {
     list.add(Arguments.of(new PolynomialF1(2, 2, 5, -18), new PolynomialF1(1, 1, 4), "2.0x-3.0"));
 
     list.add(Arguments.of(new PolynomialF1(3, 1, 1), new PolynomialF1(2, 1, 1, -2), "x"));
+
+    Arguments[] pols = new Arguments[list.size()];
+    return Stream.of(list.toArray(pols));
+  }
+
+  private static Stream<Arguments> providerForPolynomialsForCompare() {
+    List<Arguments> list = new ArrayList<Arguments>();
+    list.add(Arguments.of(new PolynomialF1(3, 1, 0, 1, 5), new PolynomialF2(2, 1, 1), false));
+
+    list.add(
+        Arguments.of(new PolynomialF1(4, 10, 9, -8, 6), new PolynomialF2(4, 1, 0, 1, 5), false));
+
+    list.add(
+        Arguments.of(new PolynomialF1(4, 1, 0, 6, 0, 6), new PolynomialF2(5, 1, 0, 6, 0, 6), true));
+
+    list.add(
+        Arguments.of(new PolynomialF1(3, 4, -7, -11, 5), new PolynomialF2(4, 4, -7, -11, 5), true));
+
+    list.add(Arguments.of(new PolynomialF1(2, 2, 5, -18), new PolynomialF2(1, 1, 4), false));
+
+    list.add(
+    Arguments.of(new PolynomialF1(3, 1, 1), new PolynomialF2(4, 1, 1, 0, 0), "x^3+2.0x^2+x-2.0"));
 
     Arguments[] pols = new Arguments[list.size()];
     return Stream.of(list.toArray(pols));
@@ -56,7 +77,6 @@ public class PolynomialF1Test {
     return Stream.of(list.toArray(pols));
   }
 
-
   @ParameterizedTest
   @MethodSource("providerForPolynomialsForDivide")
   public void testDivide(PolynomialF1 pol1, PolynomialF1 pol2, String expected) {
@@ -69,6 +89,16 @@ public class PolynomialF1Test {
   public void testAdd(PolynomialF1 pol1, PolynomialF1 pol2, String expected) {
     PolynomialF1 result = pol1.add(pol2);
     assertEquals(result.show(), expected);
+  }
+
+  @ParameterizedTest
+  @MethodSource("providerForPolynomialsForCompare")
+  public void testcomparisonPolynomialF1WithPolynomialF2(PolynomialF1 pol1, PolynomialF2 pol2,
+      boolean expected) {
+    System.out.println(pol1.show());
+    System.out.println(pol2.show());
+    boolean result = pol1.comparisonPolynomialF1WithPolynomialF2(pol2);
+    assertEquals(expected, result);
   }
 
 }
